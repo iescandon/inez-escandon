cd2cae496ed770707d9b1583eac0dff6fa8b188b
Inez Escandon <iescan4@gmail.com>
2020-08-03 13:08:06 -0500

    added side nav

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  openNav() {
+    document.getElementById('mySidenav').style.width = '250px';
   }
 
+  closeNav() {
+    document.getElementById('mySidenav').style.width = '0';
+  }
 }
